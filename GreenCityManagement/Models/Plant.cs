3e6a0f8f2fd4144e75755d397ba8a44f308fa376
ivanbiryukovv <ivanbiryukov2021@gmail.com>
2026-02-04 22:08:43 +0300

    Добавлен сценарий

--- GreenCityManagement/Models/Plant.cs
+++ GreenCityManagement/Models/Plant.cs
@@ -14,7 +14,5 @@ namespace GreenCityManagement.Models
         public int ID_district { get; set; }
         public DateTime Planting_date { get; set; }
         public string Health_status { get; set; }
-        public decimal Latitude { get; set; }
-        public decimal Longitude { get; set; }
     }
 }
